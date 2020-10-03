a0f4605072db64bcb6f0b945e3db79f78d526347
Günay Catak <adminghlabs.de>
2020-10-04 00:26:33 +0200

    added cors config

--- src/main/java/com/ghlabs/snippez/configuration/CorsConfig.java
+++ src/main/java/com/ghlabs/snippez/configuration/CorsConfig.java
@@ -1,14 +1,10 @@
 package com.ghlabs.snippez.configuration;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Arrays;
 import java.util.Collections;
